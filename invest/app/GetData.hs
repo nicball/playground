8481b127b4c8e29f684a3c0eac489270f34e3d2a
Nick <znhihgiasy@gmail.com>
2021-09-30 21:00:33 +0800

    update

--- invest/app/GetData.hs
+++ invest/app/GetData.hs
@@ -1,13 +1,13 @@
 module GetData (getData) where
 
-import Control.Lens (filtered, to, toListOf, (^..), (^?!), (%~), _1, folded, minimumOf, (-~), view)
+import Control.Lens (filtered, folded, minimumOf, to, toListOf, view, (%~), (-~), (^..), (^?!), _1, _2)
 import Data.Aeson (Value)
-import Data.Aeson.Lens (key, nth, values, _String, _Integer)
+import Data.Aeson.Lens (key, nth, values, _Integer, _String)
 import Data.Foldable (traverse_)
-import Data.List (sortOn)
 import Data.Function (on)
-import Data.Text as Text (Text, isSuffixOf, pack, unpack)
+import Data.List (groupBy, sortOn)
 import Data.Maybe (fromJust)
+import Data.Text as Text (Text, isSuffixOf, pack, unpack)
 import Database.SQLite.Simple as Sql (Connection, executeMany, execute_, open, withExclusiveTransaction)
 import Network.HTTP.Simple (getResponseBody, httpJSON, parseRequest_)
 import System.IO.Unsafe (unsafePerformIO)
@@ -36,11 +36,12 @@ getKLines symbol = do
     toRead = to (read . Text.unpack)
 
 normalize :: [(Integer, Text, Double, Double)] -> [(Integer, Text, Double, Double)]
-normalize = offsetTime . coarserTime
+normalize = offsetTime . coarserTime . ensureReliableCoin . ensureBigMarket
   where
     offsetTime = (fmap . (_1 -~)) =<< fromJust . minimumOf (folded . _1)
     coarserTime = fmap (_1 %~ (`div` (1000 * 3600)))
-    ensureBigMarket = concat . filter ((> 300) . length) . groupBy ((==) `on` (view _1)) . sortOn (view _1)
+    ensureBigMarket = concat . filter ((> 300) . length) . groupBy ((==) `on` view _1) . sortOn (view _1)
+    ensureReliableCoin = concat . filter ((> 500) . length) . groupBy ((==) `on` view _2) . sortOn (view _2)
 
 getData :: Connection -> IO ()
 getData sqlConn = do
