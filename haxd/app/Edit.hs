eed6512df02ed6e80c018788e65a66572da54c45
Nick Ballard <znhihgiasy@gmail.com>
2024-06-24 14:16:43 +0800

    Copy pasta fail. still had a instead of a

--- haxd/app/Edit.hs
+++ haxd/app/Edit.hs
@@ -12,37 +12,41 @@ import Control.Arrow ( (&&&) )
 import qualified System.IO as IO
 import System.Environment ( getEnv )
 import System.Process ( callProcess )
-import System.Posix.Files ( rename, removeLink )
+import qualified System.Posix.Files as Posix
 import Control.Exception ( finally, onException )
 import qualified Data.Text.Lazy.IO as Text
 import qualified Data.ByteString.Lazy as BS
 
 edit :: EditArgs -> IO ()
 edit args = do
-  let
-    inputPath = editInputFile args
-    (fileName, directory) = (Path.takeFileName &&& Path.takeDirectory) inputPath
-
-  withBinaryTempFile directory fileName \dumpPath dumpFile -> do
+  let inputPath = editInputFile args
+  withBinaryTempFile inputPath \dumpPath dumpFile -> do
     Text.hPutStr dumpFile
       . bytesToXXD (fromIntegral . editNumColumns $ args) (fromIntegral . editGroupSize $ args) 0
       =<< BS.readFile inputPath
     IO.hFlush dumpFile
     editor <- getEnv "EDITOR"
     callProcess editor [ dumpPath ]
-    withBinaryTempFileToPersist directory fileName inputPath \_ loadFile -> do
+    withBinaryTempFileToPersist inputPath \_ loadFile -> do
       IO.hSeek dumpFile IO.AbsoluteSeek 0
       xxd <- parseXXD <$> Text.hGetContents dumpFile
       if editPatchMode args
         then patchXXD loadFile xxd
         else BS.hPut loadFile . xxdToBytes $ xxd
 
-withBinaryTempFile :: FilePath -> String -> (FilePath -> IO.Handle -> IO a) -> IO a
-withBinaryTempFile dir template action = do
-  (path, hdl) <- IO.openBinaryTempFile dir template
-  action path hdl `finally` removeLink path `finally` IO.hClose hdl
+withBinaryTempFile :: FilePath -> (FilePath -> IO.Handle -> IO a) -> IO a
+withBinaryTempFile path action = do
+  let (dir, template) = (Path.takeDirectory &&& Path.takeFileName) path
+  (tmpPath, hdl) <- IO.openBinaryTempFile dir template
+  action tmpPath hdl `finally` Posix.removeLink tmpPath `finally` IO.hClose hdl
 
-withBinaryTempFileToPersist :: FilePath -> String -> FilePath -> (FilePath -> IO.Handle -> IO a) -> IO a
-withBinaryTempFileToPersist dir template persistPath action = do
-  (path, hdl) <- IO.openBinaryTempFile dir template
-  (action path hdl <* rename path persistPath) `onException` removeLink path `finally` IO.hClose hdl
+withBinaryTempFileToPersist :: FilePath -> (FilePath -> IO.Handle -> IO a) -> IO a
+withBinaryTempFileToPersist path action = do
+  let (dir, template) = (Path.takeDirectory &&& Path.takeFileName) path
+  (tmpPath, hdl) <- IO.openBinaryTempFile dir template
+  (action tmpPath hdl <* persist tmpPath) `onException` Posix.removeLink tmpPath `finally` IO.hClose hdl
+  where
+    persist tmpPath = do
+      mode <- Posix.fileMode <$> Posix.getFileStatus path
+      Posix.setFileMode tmpPath mode
+      Posix.rename tmpPath path
