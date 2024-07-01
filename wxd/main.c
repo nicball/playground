efe8635bffcde3d2d230b5890f7a4a10abb1a347
Nick Ballard <znhihgiasy@gmail.com>
2024-07-01 12:30:02 +0800

    Fix PC Load Letter Error

--- wxd/main.c
+++ wxd/main.c
@@ -39,7 +39,7 @@ void parse_cli(int argc, char** argv) {
 }
 
 void hex(unsigned long long value, int width, char* out) {
-  static const char to_s[0xF + 1] = "0123456789ABCDEF";
+  static const char to_s[0xF + 1] = "0123456789abcdef";
   for (int i = width - 1; i >= 0; --i) {
     int d = value & 0xF;
     value = value >> 4;
@@ -47,26 +47,30 @@ void hex(unsigned long long value, int width, char* out) {
   }
 }
 
+int min(int a, int b) { return a < b ? a : b; }
+
 void dump() {
-  char inbuf[g_num_colums];
   const int num_groups = -(g_num_colums / -g_group_size);
   const int hex_width = g_num_colums * 2 + num_groups - 1;
   const int line_width = 8 + 2 + hex_width + 2 + g_num_colums + 1;
-  const int num_outbuf_lines = 16 * 1024 / line_width;
-  const int outbuf_size = line_width * num_outbuf_lines;
-  char* const outbuf = (char*) malloc(num_outbuf_lines * line_width);
-  if (!outbuf) {
-    fputs("allocation error\n", stderr);
+  const int num_lines = 16 * 1024 / line_width;
+  const int outbuf_size = line_width * num_lines;
+  const int inbuf_size = g_num_colums * num_lines;
+  char* const inbuf = (char*) malloc(inbuf_size);
+  char* const outbuf = (char*) malloc(outbuf_size);
+  if (!outbuf || !inbuf) {
+    perror("allocation error");
     exit(EXIT_FAILURE);
   }
-  int outbuf_cursor = 0;
   size_t offset = 0;
   while (1) {
-    size_t r = fread(inbuf, 1, g_num_colums, g_input_file);
-    size_t line_no = offset / g_num_colums;
-    int base = line_no * line_width % outbuf_size;
-    int cursor = base;
-    if (r) {
+    int inbuf_len = fread(inbuf, 1, inbuf_size, g_input_file);
+    int cursor;
+    for (int inbase = 0; inbase < inbuf_len; inbase += g_num_colums) {
+      const int line_no = inbase / g_num_colums;
+      const int outbase = line_no * line_width;
+      cursor = outbase;
+      const int r = min(g_num_colums, inbuf_len - inbase);
       hex(offset, 8, &outbuf[cursor]);
       cursor += 8;
       outbuf[cursor++] = ':';
@@ -74,32 +78,31 @@ void dump() {
         if (i % g_group_size == 0) {
           outbuf[cursor++] = ' ';
         }
-        hex((unsigned char) inbuf[i], 2, &outbuf[cursor]);
+        hex((unsigned char) inbuf[inbase + i], 2, &outbuf[cursor]);
         cursor += 2;
       }
-      while (cursor < base + 10 + hex_width + 2) {
+      while (cursor < outbase + 10 + hex_width + 2) {
         outbuf[cursor++] = ' ';
       }
       for (int i = 0; i < r; ++i) {
-        outbuf[cursor++] = 0x20 <= inbuf[i] && inbuf[i] <= 0x7e ? inbuf[i] : '.';
+        outbuf[cursor++] = 0x20 <= inbuf[inbase + i] && inbuf[inbase + i] <= 0x7e ? inbuf[inbase + i] : '.';
       }
       outbuf[cursor++] = '\n';
+      offset += r;
     }
-    if (r < g_num_colums || cursor == outbuf_size) {
-      if (fwrite(outbuf, 1, cursor, g_output_file) != cursor) {
-        perror("output error");
+    if (fwrite(outbuf, 1, cursor, g_output_file) != cursor) {
+      perror("output error");
+      exit(EXIT_FAILURE);
+    }
+    if (inbuf_len < inbuf_size) {
+      if (!feof(g_input_file)) {
+        perror("input error");
         exit(EXIT_FAILURE);
       }
-      if (r < g_num_colums) {
-        if (!feof(g_input_file)) {
-          perror("input error");
-          exit(EXIT_FAILURE);
-        }
-        else break;
-      }
+      else break;
     }
-    offset += r;
   }
+  free(inbuf);
   free(outbuf);
 }
 
