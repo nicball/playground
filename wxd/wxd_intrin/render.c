423408b0411a2af2834c1aca169b1630a755f1b2
Nick Ballard <znhihgiasy@gmail.com>
2024-07-10 02:07:22 +0800

    This is not a commit

--- wxd/wxd_intrin/render.c
+++ wxd/wxd_intrin/render.c
@@ -3,18 +3,18 @@
 
 void render_ascii(const uint8_t* const inbuf, const int inbuf_len, uint8_t* const outbuf) {
   int i;
-  for (i = 0; i + 16 < inbuf_len; i += 16) {
+  for (i = 0; i + 16 <= inbuf_len; i += 16) {
     __m128i byte = _mm_loadu_si128(&inbuf[i]);
     __m128i ascii = _mm_blendv_epi8(
-      byte,
       _mm_set1_epi8('.'),
+      byte,
       _mm_and_si128(
-        _mm_cmpgt_epi8(byte, _mm_set1_epi16(' ' - 1)),
-        _mm_cmpgt_epi8(_mm_set1_epi8(0x7e + 1), byte)));
+        _mm_cmpgt_epi8(byte, _mm_set1_epi8(' ' - 1)),
+        _mm_cmpgt_epi8(_mm_set1_epi8('~' + 1), byte)));
     _mm_storeu_si128(&outbuf[i], ascii);
   }
   for(; i < inbuf_len; ++i)
-    outbuf[i] = ' ' <= inbuf[i] && inbuf[i] <= 0x7e ? inbuf[i] : '.';
+    outbuf[i] = (' ' <= inbuf[i] && inbuf[i] <= '~') ? inbuf[i] : '.';
 }
 
 static inline __m128i i16x8_to_i8x8(__m128i a) {
@@ -34,7 +34,7 @@ void render_hex(
 ) {
   int8_t base;
   int8_t out_off = 0;
-  for (base = 0; base + 8 < inbuf_len; base += 8) {
+  for (base = 0; base + 8 <= inbuf_len; base += 8) {
     __m128i byte = _mm_cvtepu8_epi16(_mm_loadu_si64(&inbuf[base]));
     __m128i byte_hi = _mm_srli_epi16(byte, 4);
     __m128i byte_lo = _mm_and_si128(byte, _mm_set1_epi16(0xF));
@@ -61,9 +61,9 @@ void render_hex(
       _mm_shuffle_epi8(nib, _mm_sub_epi8(out_rest_idx, _mm_set1_epi8(base * 2))),
       _mm_set1_epi8(' '),
       _mm_cmpeq_epi8(out_rest_idx, _mm_set1_epi8(-1)));
-    uint64_t nnib = _mm_cvtsi128_si64(nib);
+    uint64_t rest = _mm_cvtsi128_si64(out_rest);
     for (int8_t i = out_off + 16; i < out_end; ++i)
-      outbuf[i] = nnib >> (i * 8);
+      outbuf[i] = rest >> ((i - (out_off + 16)) * 8);
     out_off = out_end;
   }
   uint8_t nibs[(inbuf_len - base) * 2];
@@ -71,8 +71,8 @@ void render_hex(
     uint8_t b = inbuf[i];
     uint8_t hi = b >> 4;
     uint8_t lo = b & 0xF;
-    nibs[(i - base) * 2] = hi < 10 ? hi + '0' : hi - 10 + 'a';
-    nibs[(i - base) * 2 + 1] = lo < 10 ? lo + '0' : lo - 10 + 'a';
+    nibs[(i - base) * 2] = (hi < 10) ? (hi + '0') : (hi - 10 + 'a');
+    nibs[(i - base) * 2 + 1] = (lo < 10) ? (lo + '0') : (lo - 10 + 'a');
   }
   int8_t end = n2o[inbuf_len * 2];
   for (int8_t i = out_off; i < end; ++i) {
